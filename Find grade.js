422ab511f03293079fa2bb7e7f05f1fce51654b2
Gowtham-Mathiyazhagan <131136186+Gowtham-Mathiyazhagan@users.noreply.github.com>
2023-06-11 20:03:53 +0530

    Get mark, show the grade

---
+++ Find grade.js
@@ -0,0 +1,18 @@
+const mark=prompt('enter your mark :' );
+if(mark>90 && mark<=100)
+{
+  console.log(mark, 'A grade');
+}
+else if(mark>60 && mark<=90)
+{
+    console.log(mark, 'B grade');
+}
+else if(mark>50 && mark<=60)
+{
+  console.log(mark, 'c grade');
+}
+else
+{
+  console.log("D grade");90
+}
+
