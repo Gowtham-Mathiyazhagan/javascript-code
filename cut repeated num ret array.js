7b056b626a9d6653ed2ef3898d828d37fae284cd
Gowtham-Mathiyazhagan <131136186+Gowtham-Mathiyazhagan@users.noreply.github.com>
2023-07-02 09:34:54 +0530

    Using splice

---
+++ cut repeated num ret array.js
@@ -0,0 +1,22 @@
+// splice the single occurrence
+function removeItemOnce(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+// splice the multi occurrence
+  function removeItemAll(arr, value) {
+    var i = 0;
+    while (i < arr.length) {
+      if (arr[i] === value) {
+        arr.splice(i, 1);
+      } else {
+        i++;
+      }
+    }
+    return arr;
+  }
+  console.log(removeItemOnce([2,5,9,1,5,8,5], 5))
+  console.log(removeItemAll([2,5,9,1,5,8,5], 5))
